a4078814ba1d5dde568beadbcdf436c69e7cee29
NadeNoob <hampusviskan12@gmail.com>
2023-04-29 16:33:54 +0200

    Update HighestPossibleNumber.js

--- HighestPossibleNumber.js
+++ HighestPossibleNumber.js
@@ -1,10 +1,10 @@
-let numbers = "3303459"
+let numbers = "38673"
 
 numbers = numbers.split("");
 for(j = 0; j < numbers.length; j++) {
     for(i = 0; i < numbers.length; i++) {
-        if(numbers[j] > numbers[i]) numbers = numbers.toString().replace(numbers[i], numbers[j]).replace(numbers[j], numbers[i]);
+        if(numbers[i] > numbers[j]) numbers = numbers.toString().replace(numbers[i], numbers[j]).replace(numbers[j], numbers[i])
     }
 }
 
-console.log(numbers.split("").reverse().join("").split(",").join(""));
+console.log(numbers.split(",").join(""));
